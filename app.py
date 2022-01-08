2775ddc193787887a000ddfe205028c0c0da6e15
Brian Kropff <brian.kropff@acquia.com>
2022-01-08 09:41:19 -0800

    Add the basic functionality to make this and any other "Name generator" project.

---
+++ app.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+print("Welcome to the Band Name Generator")
+
+# Gather information on the band.
+city=input("Name a city you grow up in?\n")
+
+food=input("Name your favorite food?\n")
+
+kicker=input("Name a random object, animal, or item?\n")
+
+#Combine the element gathered together.
+print("The name of your band could be:" + city + " " + food + " " + kicker +".")
